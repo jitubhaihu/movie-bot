e76bd65aa004390719528750518b48e7f2e4c41c
jitubhaihu <jitusoniramsar@gmail.com>
2026-08-17 12:11:32 +0530

    Update info.py

--- info.py
+++ info.py
@@ -41,7 +41,7 @@ NOR_IMG = environ.get("NOR_IMG", "https://graph.org/file/e20b5fdaf217252964202.j
 MELCOW_PHOTO = environ.get("MELCOW_PHOTO", "https://graph.org/file/56b5deb73f3b132e2bb73.jpg")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/13702ae26fb05df52667c.jpg")
 SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://files.catbox.moe/s096pf.jpg'))
-FSUB_PICS = (environ.get('FSUB_PICS', 'https://graph.org/file/7478ff3eac37f4329c3d8.jpg https://graph.org/file/56b5deb73f3b132e2bb73.jpg')).split()  # Fsub pic
+FSUB_PICS = (environ.get('FSUB_PICS', 'https://files.catbox.moe/2wj4kz.jpg https://files.catbox.moe/fydkks.jpg')).split()  # Fsub pic
 
 # ============================
 # Admin, Channels & Users Configuration
